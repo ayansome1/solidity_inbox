47e448c639bc2c1c77db243e236c6e3950116060
Ayan Some <ayan@Ayans-MacBook-Pro.local>
2021-03-18 12:58:06 +0530

    init

---
+++ contracts/Inbox.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0;
+
+contract Inbox {
+    string public message;
+    constructor(string memory initialMessage) {
+        message = initialMessage;
+    }
+    function setMessage (string memory newMessage) public {
+        message = newMessage;
+    }
+}
